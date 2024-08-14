20ed689795b1bd8dcca4b20f40fbbc5d7d08b14e
Vinarasan55 <122718267+Vinarasan55@users.noreply.github.com>
2024-08-14 10:50:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -636,7 +636,7 @@ async def save_tutorial(client, message):
     try:
         tutorial = message.text.split(" ", 1)[1]
     except:
-        return await message.reply_text("<b>Command Incomplete!!\n\nuse like this -</b>\n\n<code>/set_caption <b>FileName: {file_name}</b></code>")
+        return await message.reply_text("<b>Command Incomplete!!\n\nuse like this -</b>\n\n<code>/set_caption FileName: {file_name}</code>")
         </code>")    
     await save_group_settings(grp_id, 'tutorial', tutorial)
     await message.reply_text(f"<b>Successfully changed tutorial for {title} to</b>\n\n{tutorial}", disable_web_page_preview=True)
